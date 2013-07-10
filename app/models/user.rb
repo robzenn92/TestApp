eca82927b3394b204f8743b9781b6b4972cd9bf7
robzenn92 <rob.zenn92@hotmail.it>
2013-07-10 16:02:07 +0200

    add validation and gravatar

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
 
-	attr_accessible :name, :surname, :email, :password, :password_confirmation
+	attr_accessible :name, :surname, :username, :email, :password, :password_confirmation
 
 	include Gravtastic
 
 	before_validation :prep_email
-	validates :name, :surname, :email, :password_digest, :presence => true
+	validates :name, :surname, :username, :email, :password_digest, :presence => true
 	validates :username, :uniqueness => true, :length => { minimum: 3, maximum: 18 }
 	validates :email, :uniqueness => true, :format => { with: /^[\w.+-]+@([\w]+.)+\w+$/ }
 	has_secure_password
