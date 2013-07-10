16c34c9d6a24bbd59127d4c26ca1d81c1f95c0c9
robzenn92 <rob.zenn92@hotmail.it>
2013-07-10 15:35:34 +0200

    add username and password field to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,11 @@
 class User < ActiveRecord::Base
 
-	attr_accessible :password, :password_confirmation
+	attr_accessible :name, :surname, :email, :password, :password_confirmation
 
 	include Gravtastic
 
 	before_validation :prep_email
-	validates :username, :email, :password_digest, :presence => true
+	validates :name, :surname, :email, :password_digest, :presence => true
 	validates :username, :uniqueness => true, :length => { minimum: 3, maximum: 18 }
 	validates :email, :uniqueness => true, :format => { with: /^[\w.+-]+@([\w]+.)+\w+$/ }
 	has_secure_password
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,16 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130710110143) do
+ActiveRecord::Schema.define(:version => 20130710133451) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "surname"
-    t.datetime "created_at", :null => false
-    t.datetime "updated_at", :null => false
+    t.datetime "created_at",      :null => false
+    t.datetime "updated_at",      :null => false
+    t.string   "email"
+    t.string   "username"
+    t.string   "password_digest"
   end
 
 end
